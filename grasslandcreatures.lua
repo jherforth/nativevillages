2168f116bf99d15a1f2893ccecf8df525b1b298f
jherforth <herforth.jason@gmail.com>
2025-11-01 15:01:38 -0400

    Updated grasslandcreatures.lua

--- grasslandcreatures.lua
+++ grasslandcreatures.lua
@@ -334,10 +334,35 @@ sounds = {
 		die_rotate = true,
 	},
 
+	do_custom = function(self, dtime)
+		nativevillages.mood.update_mood(self, dtime)
+		return true
+	end,
+
+	on_activate = function(self, staticdata, dtime)
+		if staticdata and staticdata ~= "" then
+			local data = minetest.deserialize(staticdata)
+			if data then
+				nativevillages.mood.on_activate_extra(self, data)
+			end
+		end
+		nativevillages.mood.init_npc(self)
+	end,
+
+	get_staticdata = function(self)
+		local mood_data = nativevillages.mood.get_staticdata_extra(self)
+		return minetest.serialize(mood_data)
+	end,
+
 	on_rightclick = function(self, clicker)
 
+		nativevillages.mood.on_interact(self, clicker)
+
 		-- feed to heal npc
-		if mobs:feed_tame(self, clicker, 8, true, true) then return end
+		if mobs:feed_tame(self, clicker, 8, true, true) then
+			nativevillages.mood.on_feed(self, clicker)
+			return
+		end
 
 		-- capture npc with net or lasso
 		if mobs:capture_mob(self, clicker, 0, 15, 25, false, nil) then return end
@@ -368,8 +393,8 @@ sounds = {
 
 			minetest.chat_send_player(name, S("Grasslander dropped you an item for bread!"))
 
+			nativevillages.mood.on_trade(self, clicker)
 			return
-		end
 
 		-- by right-clicking owner can switch npc between follow and stand
 		if self.owner and self.owner == name then
@@ -482,10 +507,35 @@ sounds = {
 		die_rotate = true,
 	},
 
+	do_custom = function(self, dtime)
+		nativevillages.mood.update_mood(self, dtime)
+		return true
+	end,
+
+	on_activate = function(self, staticdata, dtime)
+		if staticdata and staticdata ~= "" then
+			local data = minetest.deserialize(staticdata)
+			if data then
+				nativevillages.mood.on_activate_extra(self, data)
+			end
+		end
+		nativevillages.mood.init_npc(self)
+	end,
+
+	get_staticdata = function(self)
+		local mood_data = nativevillages.mood.get_staticdata_extra(self)
+		return minetest.serialize(mood_data)
+	end,
+
 	on_rightclick = function(self, clicker)
 
+		nativevillages.mood.on_interact(self, clicker)
+
 		-- feed to heal npc
-		if mobs:feed_tame(self, clicker, 8, true, true) then return end
+		if mobs:feed_tame(self, clicker, 8, true, true) then
+			nativevillages.mood.on_feed(self, clicker)
+			return
+		end
 
 		-- capture npc with net or lasso
 		if mobs:capture_mob(self, clicker, 0, 15, 25, false, nil) then return end
@@ -516,6 +566,7 @@ sounds = {
 
 			minetest.chat_send_player(name, S("Witch dropped you an item for diamond!"))
 
+			nativevillages.mood.on_trade(self, clicker)
 			return
 		end
 
