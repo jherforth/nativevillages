08b0c9748d039be7a7126311f6ef0fb27e390cb6
jherforth <herforth.jason@gmail.com>
2025-11-24 08:57:36 -0500

    Change place_offset_y to 0 for desert buildings

--- desertbuildings.lua
+++ desertbuildings.lua
@@ -3,7 +3,7 @@ local S = minetest.get_translator("nativevillages")
 -- Desert Village Buildings
 -- Schematic naming format: structurename_X_Y_Z.mts
 -- X = width, Y = height (informational), Z = depth
--- All structures use place_offset_y = -1 for ground-level placement
+-- All structures use place_offset_y = 0 for ground-level placement and can be adjusted if necessary
 -- Terrain validation: spawn_by + num_spawn_by ensures ~50% of footprint is flat ground
 
 minetest.register_decoration({
@@ -12,7 +12,7 @@ minetest.register_decoration({
     place_on = {"default:desert_sand"},
     spawn_by = {"default:desert_sand"},
     num_spawn_by = 15,
-    place_offset_y = -1,
+    place_offset_y = 0,
     sidelen = 16,
     fill_ratio = 0.0002,
     biomes = {"desert"},
@@ -29,7 +29,7 @@ minetest.register_decoration({
     place_on = {"default:desert_sand"},
     spawn_by = {"default:desert_sand"},
     num_spawn_by = 10,
-    place_offset_y = -1,
+    place_offset_y = 0,
     sidelen = 8,
     fill_ratio = 0.0002,
     biomes = {"desert"},
@@ -46,7 +46,7 @@ minetest.register_decoration({
     place_on = {"default:desert_sand"},
     spawn_by = {"default:desert_sand"},
     num_spawn_by = 10,
-    place_offset_y = -1,
+    place_offset_y = 0,
     sidelen = 8,
     fill_ratio = 0.0002,
     biomes = {"desert"},
@@ -63,7 +63,7 @@ minetest.register_decoration({
     place_on = {"default:desert_sand"},
     spawn_by = {"default:desert_sand"},
     num_spawn_by = 10,
-    place_offset_y = -1,
+    place_offset_y = 0,
     sidelen = 8,
     fill_ratio = 0.0002,
     biomes = {"desert"},
@@ -80,7 +80,7 @@ minetest.register_decoration({
     place_on = {"default:desert_sand"},
     spawn_by = {"default:desert_sand"},
     num_spawn_by = 12,
-    place_offset_y = -1,
+    place_offset_y = 0,
     sidelen = 8,
     fill_ratio = 0.0002,
     biomes = {"desert"},
@@ -97,7 +97,7 @@ minetest.register_decoration({
     place_on = {"default:desert_sand"},
     spawn_by = {"default:desert_sand"},
     num_spawn_by = 20,
-    place_offset_y = -1,
+    place_offset_y = 0,
     sidelen = 16,
     fill_ratio = 0.0001,
     biomes = {"desert"},
@@ -114,7 +114,7 @@ minetest.register_decoration({
     place_on = {"default:desert_sand"},
     spawn_by = {"default:desert_sand"},
     num_spawn_by = 25,
-    place_offset_y = -1,
+    place_offset_y = 0,
     sidelen = 16,
     fill_ratio = 0.00015,
     biomes = {"desert"},
@@ -131,7 +131,7 @@ minetest.register_decoration({
     place_on = {"default:desert_sand"},
     spawn_by = {"default:desert_sand"},
     num_spawn_by = 18,
-    place_offset_y = -1,
+    place_offset_y = 0,
     sidelen = 16,
     fill_ratio = 0.00012,
     biomes = {"desert"},
@@ -141,3 +141,4 @@ minetest.register_decoration({
     flags = "place_center_x, place_center_z, force_placement",
     rotation = "random",
 })
+
