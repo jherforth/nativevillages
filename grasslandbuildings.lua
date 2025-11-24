b4b32871ebf858488eff23aa50d55db2da3e27c4
jherforth <herforth.jason@gmail.com>
2025-11-24 09:34:11 -0500

    Update grasslandbuildings.lua

--- grasslandbuildings.lua
+++ grasslandbuildings.lua
@@ -99,7 +99,7 @@ minetest.register_decoration({
     num_spawn_by = 50,
     place_offset_y = 1,
     sidelen = 32,
-    fill_ratio = 0.0001,
+    fill_ratio = 0.0002,
     biomes = {"grassland", "snowy_grassland"},
     y_max = 50,
     y_min = 1,
@@ -116,7 +116,7 @@ minetest.register_decoration({
     num_spawn_by = 20,
     place_offset_y = 1,
     sidelen = 16,
-    fill_ratio = 0.00015,
+    fill_ratio = 0.0002,
     biomes = {"grassland", "snowy_grassland"},
     y_max = 50,
     y_min = 1,
@@ -133,7 +133,7 @@ minetest.register_decoration({
     num_spawn_by = 50,
     place_offset_y = 1,
     sidelen = 32,
-    fill_ratio = 0.00012,
+    fill_ratio = 0.0002,
     biomes = {"grassland", "snowy_grassland"},
     y_max = 50,
     y_min = 1,
@@ -141,6 +141,7 @@ minetest.register_decoration({
     flags = "place_center_x, place_center_z, force_placement",
     rotation = "random",
 })
+
 
 
 
