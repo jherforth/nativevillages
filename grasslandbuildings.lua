40708bf56ab3d513f2671746975f2c2315458bf8
jherforth <herforth.jason@gmail.com>
2025-11-26 14:11:46 -0500

    Update grasslandbuildings.lua

--- grasslandbuildings.lua
+++ grasslandbuildings.lua
@@ -17,12 +17,13 @@ local function register_grassland_building(params)
         name = "nativevillages:" .. params.name,
         deco_type = "schematic",
         place_on = {"default:dirt_with_grass", "default:dirt_with_coniferous_litter"},
-        sidelen = 24,
+        sidelen = 32,
         noise_params = village_noise,
         biomes = {"grassland", "coniferous_forest", "deciduous_forest"},
         y_min = 0,
         y_max = 110,                    -- Higher than desert — forests go up to ~100
-        height = 2,                     -- Grassland is flatter → we can demand flatter ground
+        height = 1,                     -- Grassland is flatter → we can demand flatter ground
+        height_max = 1,
         place_offset_y = params.offset_y or 0,
         schematic = minetest.get_modpath("nativevillages") .. "/schematics/" .. params.file,
         flags = "place_center_x, place_center_z, force_placement",
@@ -50,12 +51,13 @@ local function register_grassland_central(params)
         name = "nativevillages:" .. params.name,
         deco_type = "schematic",
         place_on = {"default:dirt_with_grass", "default:dirt_with_coniferous_litter"},
-        sidelen = 32,
+        sidelen = 40,
         noise_params = central_noise,
         biomes = {"grassland", "snowy_grassland", "coniferous_forest", "deciduous_forest"},
         y_min = 1,
         y_max = 110,
-        height = 2,
+        height = 1,
+        height_max = 1,
         place_offset_y = params.offset_y or 0,
         schematic = minetest.get_modpath("nativevillages") .. "/schematics/" .. params.file,
         flags = "place_center_x, place_center_z, force_placement",
@@ -66,6 +68,7 @@ end
 register_grassland_central({name = "grasslandchurch", file = "grasslandchurch_11_17_21.mts", offset_y = 0})
 register_grassland_central({name = "grasslandmarket",file = "grasslandmarket_9_5_9.mts", offset_y = 0})
 register_grassland_central({name = "grasslandstable", file = "grasslandstable_15_8_16.mts", offset_y = 0})
+
 
 
 
