490ef4c85d34ae6c05fdce76c7bf7a9f6d7026b7
jherforth <herforth.jason@gmail.com>
2025-11-26 16:33:02 -0500

    Update utils.lua

--- utils.lua
+++ utils.lua
@@ -1,70 +1,61 @@
--- utils.lua  –  SAFE version that only fills under the actual house footprint
+-- utils.lua  –  FINAL safe version (2025 standard)
 
-nativevillages.fill_under_house = function(pos, schem_path_or_size)
-    -- 1. Determine the actual size of the schematic that was just placed
-    local size = {x = 20, y = 15, z = 20}  -- sensible default if we can't detect
-
-    if schem_path_or_size then
-        if type(schem_path_or_size) == "string" then
-            -- It's a path → read the real size
-            local filepath = schem_path_or_size
-            if minetest.get_modpath("nativevillages") then
-                filepath = minetest.get_modpath("nativevillages") .. "/schematics/" .. schem_path_or_size:gsub("%.mts$","") .. ".mts"
-            end
-            size = minetest.get_schematic_size(filepath) or size
-        elseif type(schem_path_or_size) == "table" then
-            size = schem_path_or_size
-        end
+nativevillages.fill_under_house = function(pos, schematic_filename)
+    -- Get real schematic size
+    local fullpath = minetest.get_modpath("nativevillages") .. "/schematics/" .. schematic_filename
+    local size = minetest.get_schematic_size(fullpath)
+    if not size or size.x == 0 then
+        size = {x = 16, y = 12, z = 16}  -- safe fallback
     end
 
-    -- 2. Add a small padding so corners are always covered
-    local padding = 3
+    -- Padding so corners and edges are always covered
+    local pad = 4
     local minp = {
-        x = pos.x - math.floor(size.x/2) - padding,
-        y = pos.y - 20,                     -- max 20 nodes down is more than enough
-        z = pos.z - math.floor(size.z/2) - padding
+        x = pos.x - math.floor(size.x/2) - pad,
+        y = pos.y - 20,        -- max 20 nodes down is plenty
+        z = pos.z - math.floor(size.z/2) - pad
     }
     local maxp = {
-        x = pos.x + math.floor(size.x/2) + padding,
-        y = pos.y + 5,                      -- a few nodes above in case of basement
-        z = pos.z + math.floor(size.z/2) + padding
+        x = pos.x + math.floor(size.x/2) + pad,
+        y = pos.y + 6,         -- slight upward buffer
+        z = pos.z + math.floor(size.z/2) + pad
     }
 
-    -- 3. Detect correct fill material (sand, dirt, snow, etc.)
-    local ground_node = minetest.get_node({x=pos.x, y=pos.y-1, z=pos.z})
-    local fill_name = "default:dirt"
-    if minetest.get_item_group(ground_node.name, "sand") > 0 then
-        fill_name = "default:desert_sand"
-    elseif ground_node.name == "default:snowblock" or ground_node.name == "default:ice" then
-        fill_name = "default:dirt"
-    elseif ground_node.name == "default:dirt_with_dry_grass" then
-        fill_name = "default:dry_dirt"
+    -- Detect correct fill material
+    local ground = minetest.get_node({x=pos.x, y=pos.y-1, z=pos.z}).name
+    local fill = "default:dirt"
+    if minetest.get_item_group(ground, "sand") > 0 or ground == "default:desert_sand" or ground == "default:sand" then
+        fill = "default:desert_sand"
+    elseif ground == "default:snowblock" or ground == "default:ice" then
+        fill = "default:dirt"
+    elseif ground:find("dry") then
+        fill = "default:dry_dirt"
     end
 
-    -- 4. Fill only air → solid fill, and stop at natural ground
+    -- Fill only air below the house, stop at natural ground
     minetest.emerge_area(minp, maxp)
-    minetest.after(0.5, function()
+    minetest.after(0.3, function()
         local vm, emin, emax = minetest.get_mapgen_object("voxelmanip")
         if not vm then return end
         local data = vm:get_data()
-        local area = VoxelArea:new{MinEdge = emin, MaxEdge = emax}
-        local fill_id = minetest.get_content_id(fill_name)
-        local air_id   = minetest.get_content_id("air")
+        local area = VoxelArea:new{MinEdge=emin, MaxEdge=emax}
+        local fill_id = minetest.get_content_id(fill)
+        local air_id = minetest.get_content_id("air")
 
         for x = minp.x, maxp.x do
             for z = minp.z, maxp.z do
-                local found_ground = false
+                local found_surface = false
                 for y = maxp.y, minp.y, -1 do
                     local vi = area:index(x, y, z)
-                    if data[vi] and data[vi] ~= air_id and data[vi] ~= minetest.get_content_id("ignore") then
-                        found_ground = true          -- we hit natural terrain or the house itself
-                    elseif found_ground and data[vi] == air_id then
-                        data[vi] = fill_id           -- fill the gap underneath
+                    local nid = data[vi]
+                    if nid and nid ~= air_id and nid ~= minetest.get_content_id("ignore") then
+                        found_surface = true
+                    elseif found_surface and nid == air_id then
+                        data[vi] = fill_id
                     end
                 end
             end
         end
-
         vm:set_data(data)
         vm:write_to_map()
     end)
