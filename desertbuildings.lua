96cc8cef11cbbdbc4f62d97e77e97028d0d3b3b3
Jay <herforth.jason@gmail.com>
2026-01-03 10:18:01 -0500

    Update desertbuildings.lua

--- desertbuildings.lua
+++ desertbuildings.lua
@@ -18,9 +18,9 @@ local function register_desert_building(params)
         noise_params = village_noise,
         biomes = {"desert","mesa","everness:forsaken_desert"},
         y_min = 1,
-        y_max = 50,
+        y_max = 2050,
 
-        place_offset_y = 0,
+        place_offset_y = -6,
         flags = "place_center_x, place_center_z, force_placement, all_floors",
         height = 0,
         height_max = 0,
@@ -43,9 +43,9 @@ local function register_desert_central(params)
         noise_params = central_noise,
         biomes = {"desert","mesa","everness:forsaken_desert"},
         y_min = 0,
-        y_max = 50,
+        y_max = 2050,
 
-        place_offset_y = 0,
+        place_offset_y = -6,
         flags = "place_center_x, place_center_z, force_placement, all_floors",
         height = 0,
         height_max = 0,
@@ -67,5 +67,6 @@ register_desert_building({name = "deserthouse4", file = "deserthouse4.mts"})
 register_desert_central({name = "desertchurch",  file = "desertchurch.mts"})
 register_desert_central({name = "desertmarket",  file = "desertmarket.mts"})
 register_desert_central({name = "desertstable",  file = "desertstable.mts"})
+
 
 
